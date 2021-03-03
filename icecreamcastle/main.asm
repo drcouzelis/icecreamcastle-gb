aa108b1cbea5b93c7787af8e780bccf2ba5de7f3
David Couzelis <drcouzelis@gmail.com>
2021-03-02 22:51:25 -0500

    Added my first sprite to the screen.

--- icecreamcastle/main.asm
+++ icecreamcastle/main.asm
@@ -8,7 +8,7 @@ INCLUDE "hardware.inc"
 ; Header
 ; Memory type: ROM 0
 ; Game execution begins at address 100
-SECTION "Header", ROM0[$100]
+SECTION "Header", ROM0[$0100]
 
 EntryPoint:
     di ; Disable interrupts (to avoid needing to deal with them for now)
@@ -19,7 +19,7 @@ REPT $150 - @
     db 0
 ENDR
 
-SECTION "Game code", ROM0
+SECTION "Game code", ROM0[$0150]
 
 Start:
     ; Turn off the LCD
@@ -31,19 +31,20 @@ Start:
     xor a ; (ld a, 0) Reset bit 7 to turn off the screen
     ld [rLCDC], a
 
+; Load level tiles
     ld hl, $9000
     ld de, Resources.background
     ld bc, Resources.endBackground - Resources.background
-.copyTiles
+.copyBackgroundTiles
     ld a, [de] ; Grab 1 byte from the source
     ld [hli], a ; Place it at the destination, incrementing hl
     inc de ; Move to the next byte
     dec bc ; Decrement count
     ld a, b ; 'dec bc' doesn't update flags, so this line...
     or c ; ...and this line check if bc is 0
-    jr nz, .copyTiles
+    jr nz, .copyBackgroundTiles
 
-; Load Level 1 background
+; Load level
     ld hl, $9800 ; The top-left corner of the screen
     ld de, Resources.level1
     ld bc, Resources.endLevel1 - Resources.level1
@@ -56,21 +57,58 @@ Start:
     or c
     jr nz, .loadBG
 
-    ; Init display registers
-    ld a, %00011011 ; Palette, first number is text, last number is background
-    ld [rBGP], a
-
     ; Set the X, Y position of the background
     xor a ; (ld a, 0)
     ld [rSCY], a
     ld [rSCX], a
+
+; Load sprite tiles
+    ld hl, $8000
+    ld de, Resources.sprites
+    ld bc, Resources.endSprites - Resources.sprites
+.copySpriteTiles
+    ld a, [de] ; Grab 1 byte from the source
+    ld [hli], a ; Place it at the destination, incrementing hl
+    inc de ; Move to the next byte
+    dec bc ; Decrement count
+    ld a, b ; 'dec bc' doesn't update flags, so this line...
+    or c ; ...and this line check if bc is 0
+    jr nz, .copySpriteTiles
+
+; Clear sprites
+    ld hl, $FE00 ; OAM start
+    ld bc, $FE9F - $FE00 ; ...OAM end
+.clearSprites
+    xor a
+    ld [hli], a
+    dec bc
+    ld a, b
+    or c
+    jr nz, .clearSprites
+
+; Load sprites
+    ; The hero
+    ld a, 136 ; Y position
+    ld [$FE00], a
+    ld a, 48 ; X position
+    ld [$FE01], a
+    ld a, $00 ; Tile number
+    ld [$FE02], a
+    ld a, %00000000 ; Attributes
+    ld [$FE03], a
+
+    ; Init palettes
+    ld a, %00011011 ; Palette, first number is text, last number is background
+    ld [rBGP], a
+    ;ld a, %01101100
+    ld [rOBP0], a
 
     ; Turn off sound
     xor a ; (ld a, 0)
     ld [rNR52], a
 
     ; Turn screen on, display the background
-    ld a, %10000001
+    ld a, %10000011
     ld [rLCDC], a
 
     ; Trap the CPU in an infinite loop
