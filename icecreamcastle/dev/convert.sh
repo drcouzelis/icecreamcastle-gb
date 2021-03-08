658191e8c6c6dc34d9508f11adb224129fd4e03e
David Couzelis <drcouzelis@gmail.com>
2021-03-08 09:34:12 -0500

    Updated script to convert PNG to GB image data.

--- icecreamcastle/dev/convert.sh
+++ icecreamcastle/dev/convert.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env bash
+
+# Sprites
+rgbgfx --output ../res/tiles-sprites.2bpp tiles-sprites.png 
+
+# Background map and tiles
 rgbgfx --unique-tiles --output ../res/tiles-background.2bpp --tilemap ../res/tilemap-level1.map tilemap-level1.png
