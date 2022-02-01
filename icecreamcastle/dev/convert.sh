4754f08ed1a6b6c993a5e17cdc5c5457a199f4f6
David Couzelis <drcouzelis@gmail.com>
2022-01-31 22:21:41 -0500

    Fixed the convert script

--- icecreamcastle/dev/convert.sh
+++ icecreamcastle/dev/convert.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Sprites
-rgbgfx --output ../res/tiles-sprites.2bpp tiles-sprites.png 
+rgbgfx --output ../resources/tiles-sprites.2bpp tiles-sprites.png 
 
 # Background map and tiles
-rgbgfx --unique-tiles --output ../res/tiles-background.2bpp --tilemap ../res/tilemap-level-01.map tilemap-level-01.png
+rgbgfx --unique-tiles --output ../resources/tiles-background.2bpp --tilemap ../resources/tilemap-level-01.map tilemap-level-01.png
