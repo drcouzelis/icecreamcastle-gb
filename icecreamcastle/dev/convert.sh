e694b6530b819f1e8d318fbdfe9b0c513824b302
David Couzelis <drcouzelis@gmail.com>
2022-01-31 22:08:18 -0500

    Renamed resources.

--- icecreamcastle/dev/convert.sh
+++ icecreamcastle/dev/convert.sh
@@ -4,4 +4,4 @@
 rgbgfx --output ../res/tiles-sprites.2bpp tiles-sprites.png 
 
 # Background map and tiles
-rgbgfx --unique-tiles --output ../res/tiles-background.2bpp --tilemap ../res/tilemap-level1.map tilemap-level1.png
+rgbgfx --unique-tiles --output ../res/tiles-background.2bpp --tilemap ../res/tilemap-level-01.map tilemap-level-01.png
